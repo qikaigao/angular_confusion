16b707cc359a0b912ce3f8cf35c5fbe62836fa39
qikaigao <qikaigao2017@gmail.com>
2020-01-11 11:44:48 -0800

    Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -6,13 +6,27 @@ import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+        transform: 'scale(1.0)',
+        opacity: 1
+      })),
+      state('hidden', style({
+        transform: 'scale(0.5)',
+        opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 
 
@@ -26,6 +40,7 @@ export class DishdetailComponent implements OnInit {
   comment: Comment;
   commentForm: FormGroup;
   dishcopy: Dish;
+  visibility = 'shown';
   @ViewChild('cform', { static: false }) commentFormDirective;
 
   formErrors = {
@@ -57,8 +72,8 @@ export class DishdetailComponent implements OnInit {
 
     this.dishservice.getDishIds()
       .subscribe(dishIds => this.dishIds = dishIds);
-    this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); }, errMess => this.errMess = <any>errMess);
+    this.route.params.pipe(switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishservice.getDish(params['id']); }))
+      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown' }, errMess => this.errMess = <any>errMess);
 
   }
 
@@ -113,10 +128,10 @@ export class DishdetailComponent implements OnInit {
     console.log(this.comment);
     this.dishcopy.comments.push(this.comment);
     this.dishservice.putDish(this.dishcopy)
-      .subscribe(dish =>{
+      .subscribe(dish => {
         this.dish = dish;
         this.dishcopy = dish;
-      }, errMess =>{
+      }, errMess => {
         this.dish = null;
         this.dishcopy = null;
         this.errMess = <any>errMess;
