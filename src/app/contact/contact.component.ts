4c9063b6680895fa0ef30fa964ecf27c35104a7f
qikaigao <qikaigao2017@gmail.com>
2020-01-11 12:35:46 -0800

    Animations Part 2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,11 +2,20 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Feedback, ContactType } from '../shared/feedback';
 import { validateBasis } from '@angular/flex-layout';
+import { flyInOut } from '../animations/app.animation';
+
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
+  animations: [
+    flyInOut()
+  ]
 })
 export class ContactComponent implements OnInit {
   feedbackForm: FormGroup;
