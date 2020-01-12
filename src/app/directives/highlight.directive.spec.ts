619850b9be50827d7847cbe075014c79ec02015d
qikaigao <qikaigao2017@gmail.com>
2020-01-12 14:28:19 -0800

    Component Test

--- src/app/directives/highlight.directive.spec.ts
+++ src/app/directives/highlight.directive.spec.ts
@@ -1,8 +1,11 @@
 import { HighlightDirective } from './highlight.directive';
+import { ElementRef, Renderer2 } from '@angular/core';
 
 describe('HighlightDirective', () => {
   it('should create an instance', () => {
-    const directive = new HighlightDirective();
+    let el:ElementRef;
+    let render:Renderer2;
+    const directive = new HighlightDirective(el, render);
     expect(directive).toBeTruthy();
   });
 });
